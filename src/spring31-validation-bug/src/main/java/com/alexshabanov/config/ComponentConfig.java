db5ad8d1559402ee94996252d5c84831a6181e86
Alexander Shabanov <avshabanov@gmail.com>
2012-01-16 23:11:50 +0300

    Component config separated

---
+++ src/spring31-validation-bug/src/main/java/com/alexshabanov/config/ComponentConfig.java
@@ -0,0 +1,11 @@
+package com.alexshabanov.config;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: alex
+ * Date: 1/16/12
+ * Time: 11:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ComponentConfig {
+}
