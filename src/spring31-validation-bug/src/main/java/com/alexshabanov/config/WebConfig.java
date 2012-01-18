f217247da0ab0665317c87ff0f26eaa9b986016e
Alexander Shabanov <avshabanov@gmail.com>
2012-01-18 18:01:19 +0400

    Simplifying

--- src/spring31-validation-bug/src/main/java/com/alexshabanov/config/WebConfig.java
+++ src/spring31-validation-bug/src/main/java/com/alexshabanov/config/WebConfig.java
@@ -1,43 +1,25 @@
 package com.alexshabanov.config;
 
 import org.codehaus.jackson.map.annotate.JsonSerialize;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
-import org.springframework.validation.Validator;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurationSupport {
-    @Autowired
-    private RequestMappingHandlerMapping requestHandlerMapping;
-
-    @PostConstruct
-    public void initHandlerMapping() {
-        requestHandlerMapping.setAlwaysUseFullPath(true);
-    }
-
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         final MappingJacksonHttpMessageConverter jacksonConverter = new MappingJacksonHttpMessageConverter();
         jacksonConverter.getObjectMapper().setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
         converters.add(jacksonConverter);
-    }
-
-    @Override
-    public Validator getValidator() {
-        return new LocalValidatorFactoryBean();
     }
 
     @Bean
@@ -48,7 +30,6 @@ public class WebConfig extends WebMvcConfigurationSupport {
         viewResolver.setSuffix(".jsp");
         return viewResolver;
     }
-
 
     @Bean
     public RequestMappingHandlerMapping requestMappingHandlerMapping() {
